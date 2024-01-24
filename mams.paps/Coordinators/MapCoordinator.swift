9fb5e756b93cc1c0bc71a33d6bfb212c0931d673
ri.gayfullin <ri.gayfullin@tensor.ru>
2024-01-24 22:41:51 +0300

    fix map

--- mams.paps/Coordinators/MapCoordinator.swift
+++ mams.paps/Coordinators/MapCoordinator.swift
@@ -14,7 +14,7 @@ final class MapCoordinator {
     
     private func createNavigationController() -> UIViewController {
 //        let viewModel = MapViewModel(coordinator: self)
-        let mapViewController = YandexMapViewController(/*viewModel: viewModel*/)
+        let mapViewController = YandexMapViewController()
         let navigationController = UINavigationController(rootViewController: mapViewController)
         navigationController.tabBarItem = UITabBarItem(title: "Map.Title".localized, image: UIImage(systemName: "map"), tag: 1)
         self.navigationController = navigationController
