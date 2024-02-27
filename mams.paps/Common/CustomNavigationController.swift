7dffaf10506d7e8cf5e88fb6b71be84ad8216725
JK <kagirova.y@inbox.ru>
2024-02-27 22:04:16 +0300

    добавила файл с кеглем шрифта, поменяла иконки, переделала кнопки, закруглила хэдэр и баттонс коллекшнвью

---
+++ mams.paps/Common/CustomNavigationController.swift
@@ -0,0 +1,8 @@
+//
+//  CustomNavigationController.swift
+//  mams.paps
+//
+//  Created by Юлия Кагирова on 22.02.2024.
+//
+
+import Foundation
---
+++ mams.paps/Common/Font.swift
@@ -0,0 +1,8 @@
+//
+//  Font.swift
+//  mams.paps
+//
+//  Created by Юлия Кагирова on 24.02.2024.
+//
+
+import Foundation
